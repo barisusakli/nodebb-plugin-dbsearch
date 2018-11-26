a940ecbd9fc6e5e6d7270fa693afb182d6ffd658
Baris Usakli <barisusakli@gmail.com>
2018-11-26 13:41:49 -0500

    closes #46

---
+++ upgrades/dbsearch_change_mongodb_schema.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+var async = require.main.require('async');
+var winston = require.main.require('winston');
+var db = module.parent.require('./database');
+
+module.exports = {
+	name: 'Changing dbsearch mongodb search schema to use _id',
+	timestamp: Date.UTC(2018, 12, 26),
+	method: function (callback) {
+		const nconf = require.main.require('nconf');
+		var isMongo = nconf.get('database') === 'mongo';
+		var progress = this.progress;
+		if (!isMongo) {
+			return callback();
+		}
+
+		const plugin = require('../lib/dbsearch');
+		var client = db.client;
+		async.series([
+			function (next) {
+				client.collection('searchtopic').removeMany({}, next);
+			},
+			function (next) {
+				client.collection('searchpost').removeMany({}, next);
+			},
+			function (next) {
+				client.collection('searchtopic').dropIndex({id: 1}, function (err) {
+					if (err) {
+						winston.error(err);
+					}
+					next();
+				});
+			},
+			function (next) {
+				client.collection('searchpost').dropIndex({id: 1}, function (err) {
+					if (err) {
+						winston.error(err);
+					}
+					next();
+				});
+			},
+			function (next) {
+				plugin.reindex(next);
+			},
+			function (next) {
+				client.collection('searchtopic').reIndex(next);
+			},
+			function (next) {
+				client.collection('searchpost').reIndex(next);
+			},
+		], callback);
+	},
+};
