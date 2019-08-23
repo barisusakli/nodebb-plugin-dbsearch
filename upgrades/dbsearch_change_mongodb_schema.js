83867667499a5c7a20295d3b6901efdb58d077f6
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-08-22 20:38:28 -0400

    fix: async/await upgrade script

--- upgrades/dbsearch_change_mongodb_schema.js
+++ upgrades/dbsearch_change_mongodb_schema.js
@@ -1,23 +1,23 @@
 'use strict';
 
 
-var async = require.main.require('async');
-var winston = require.main.require('winston');
-var db = module.parent.require('./database');
+const async = require.main.require('async');
+const winston = require.main.require('winston');
+const db = module.parent.require('./database');
 
 module.exports = {
 	name: 'Changing dbsearch mongodb search schema to use _id',
 	timestamp: Date.UTC(2018, 10, 26),
 	method: function (callback) {
 		const nconf = require.main.require('nconf');
-		var isMongo = nconf.get('database') === 'mongo';
-		var progress = this.progress;
+		const isMongo = nconf.get('database') === 'mongo';
 		if (!isMongo) {
 			return callback();
 		}
 
 		const plugin = require('../lib/dbsearch');
-		var client = db.client;
+		const client = db.client;
+
 		async.series([
 			function (next) {
 				client.collection('searchtopic').removeMany({}, next);
@@ -26,7 +26,7 @@ module.exports = {
 				client.collection('searchpost').removeMany({}, next);
 			},
 			function (next) {
-				client.collection('searchtopic').dropIndex({id: 1}, function (err) {
+				client.collection('searchtopic').dropIndex({ id: 1 }, function (err) {
 					if (err) {
 						winston.error(err);
 					}
@@ -34,15 +34,15 @@ module.exports = {
 				});
 			},
 			function (next) {
-				client.collection('searchpost').dropIndex({id: 1}, function (err) {
+				client.collection('searchpost').dropIndex({ id: 1 }, function (err) {
 					if (err) {
 						winston.error(err);
 					}
 					next();
 				});
 			},
-			function (next) {
-				plugin.reindex(next);
+			async function () {
+				await plugin.reindex();
 			},
 		], callback);
 	},
