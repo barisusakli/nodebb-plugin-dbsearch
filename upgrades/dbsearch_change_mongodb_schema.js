ca199e81fa20013ae6b5e10045bfea27a7f462b9
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-09-23 15:34:23 -0400

    fix: removeMany=>deleteMany

--- upgrades/dbsearch_change_mongodb_schema.js
+++ upgrades/dbsearch_change_mongodb_schema.js
@@ -1,49 +1,28 @@
 'use strict';
 
 
-const async = require.main.require('async');
 const winston = require.main.require('winston');
 const db = module.parent.require('./database');
 
 module.exports = {
 	name: 'Changing dbsearch mongodb search schema to use _id',
 	timestamp: Date.UTC(2018, 10, 26),
-	method: function (callback) {
+	method: async function () {
 		const nconf = require.main.require('nconf');
 		const isMongo = nconf.get('database') === 'mongo';
 		if (!isMongo) {
-			return callback();
+			return;
 		}
 
 		const plugin = require('../lib/dbsearch');
-		const client = db.client;
-
-		async.series([
-			function (next) {
-				client.collection('searchtopic').removeMany({}, next);
-			},
-			function (next) {
-				client.collection('searchpost').removeMany({}, next);
-			},
-			function (next) {
-				client.collection('searchtopic').dropIndex({ id: 1 }, function (err) {
-					if (err) {
-						winston.error(err);
-					}
-					next();
-				});
-			},
-			function (next) {
-				client.collection('searchpost').dropIndex({ id: 1 }, function (err) {
-					if (err) {
-						winston.error(err);
-					}
-					next();
-				});
-			},
-			async function () {
-				await plugin.reindex();
-			},
-		], callback);
+		await db.client.collection('searchtopic').deleteMany({});
+		await db.client.collection('searchpost').deleteMany({});
+		try {
+			await db.client.collection('searchtopic').dropIndex({ id: 1 });
+			await db.client.collection('searchpost').dropIndex({ id: 1 });
+		} catch (err) {
+			winston.error(err.stack);
+		}
+		await plugin.reindex();
 	},
 };
